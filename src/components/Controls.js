31387e04d507ca99a6574f78efbf191529de1f19
Michael Caterisano <mcaterisano@gmail.com>
2019-12-01 15:19:26 -0500

    add sound message

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -8,6 +8,7 @@ import yellow from "../assets/yellow.png";
 import red from "../assets/red.png";
 import { Slider } from "@material-ui/core";
 import { Button } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 
 import "./controls.css";
 
@@ -81,74 +82,68 @@ class Controls extends React.Component {
   render() {
     const synth = this.props.synth;
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100%",
-          backgroundColor: `hsl(${this.state.shiftedHue}, 100%, 50%)`
-        }}
-      >
-        {/* <div style={{ height: "100vh" }}>
-          <Text
-            hue={this.state.hue}
-            shiftedHue={this.state.shiftedHue}
-            lightness={this.state.lightness}
-            frequency={this.state.frequency}
-          />
-        </div> */}
-
-        {/* Blue */}
-        <div className="wrapper">
-          <img src={blue} alt=""></img>
-        </div>
-
-        {/* Controls */}
+      <React.Fragment>
         <div
           style={{
-            width: "80vw",
             display: "flex",
-            flexDirection: "column",
             justifyContent: "center",
-            alignItems: "center"
+            alignItems: "center",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100%",
+            backgroundColor: `hsl(${this.state.shiftedHue}, 100%, 50%)`
           }}
         >
-          <div id="buttons" style={{ display: "flex" }}>
-            <div style={{ margin: "10px" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => synth.start()}
-              >
-                start
-              </Button>
-            </div>
-
-            <div style={{ margin: "10px" }}>
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => synth.stop()}
-              >
-                stop
-              </Button>
-            </div>
+          <Typography color="secondary">sound on please</Typography>
+          {/* Blue */}
+          <div className="wrapper">
+            <img src={blue} alt=""></img>
           </div>
+          {/* Controls */}
+          <div
+            style={{
+              width: "80vw",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            {" "}
+            <div id="buttons" style={{ display: "flex" }}>
+              <div style={{ margin: "10px" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => synth.start()}
+                >
+                  start
+                </Button>
+              </div>
 
-          <div id="slider-container" style={{ width: "80%" }}>
-            <Slider
-              min={0}
-              max={255}
-              step={0.01}
-              defaultValue={red}
-              track={false}
-              onChange={this.onHueChange}
-            />
+              <div style={{ margin: "10px" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => synth.stop()}
+                >
+                  stop
+                </Button>
+              </div>
+            </div>
+            <div id="slider-container" style={{ width: "80%" }}>
+              <Slider
+                min={0}
+                max={255}
+                step={0.01}
+                defaultValue={red}
+                track={false}
+                onChange={this.onHueChange}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
